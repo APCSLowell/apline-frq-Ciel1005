93fc33d712b8efdd83dadf305c33607239efcc0f
Ciel1005 <143016585+Ciel1005@users.noreply.github.com>
2024-04-22 16:17:38 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,20 @@
 public class APLine
 {
   /* your code here */
-  
+  private int a, b, c;
+  public APLine(int x, int y, int z){
+    a = x;
+    b = y;
+    c = z;
+  }
+  public double getSlope(){
+  return -a/b;
+  }
+
+  public boolean isOnLine(int x, int y){
+  int line = (a*x) + (b*y) + c;
+    if(line == 0)
+      return true;
+    return false;
+  }
 }
